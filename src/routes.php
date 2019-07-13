be6c1826020e4afa5b4027db91548d2cb83a8bdd
Evan Coury <me@evancoury.com>
2019-07-12 22:11:25 -0700

    Allow specifying mission in email csv url

--- src/routes.php
+++ src/routes.php
@@ -466,9 +466,9 @@ email;
         die();
 
     });
-    $app->get('/admin/stp-2-emails', function (Request $request, Response $response, array $args) use ($container, $currentMission) {
+    $app->get('/admin/{mission}/emails', function (Request $request, Response $response, array $args) use ($container) {
         if (!$container->get('session')->exists('admin')) return $response->withRedirect('/admin/login');
-        $reservationKeys = $container->get('redis')->zRangeByScore("$currentMission:reservations", '-inf', '+inf');
+        $reservationKeys = $container->get('redis')->zRangeByScore("{$args['mission']}:reservations", '-inf', '+inf');
         header('Content-Type:text/plain');
         foreach ($reservationKeys as $reservationKey) {
             $reservation = $container->get('redis')->hGetAll($reservationKey);
