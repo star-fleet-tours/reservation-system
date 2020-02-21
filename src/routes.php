7612921161ec240bdd9254a1951920f0eee46250
Evan Coury <me@evancoury.com>
2020-02-20 18:54:17 -0700

    Updates for CRS-20

--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,7 @@ use Slim\Http\Response;
 use PHPMailer\PHPMailer\PHPMailer;
 
 return function (App $app) {
-    $currentMission = 'atlas-v11';
+    $currentMission = 'crs-20';
     $container = $app->getContainer();
 
     $inventoryCheck = new class($container, $currentMission) {
